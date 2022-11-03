099f29e83677a665dc6fde4ce6e9e92d48e6f07b
Suhankov Vlad <suhankovg@gmail.com>
2022-11-03 20:50:59 +0700

    fix(authorization): FIxed token expiration time
    The token expired for at least 5 minutes even if real time was less

--- MovieAppAPI/Config/TokenConfig.cs
+++ MovieAppAPI/Config/TokenConfig.cs
@@ -7,7 +7,7 @@ public static class TokenConfig {
     public const string Issuer = "MovieAppServerHITs";      // Token creator
     public const string Audience = "MovieAppClientHITs";    // Token consumer
     private const string Key = "some12S4__secret)))KeYYY";  // Secret key
-    public const int Lifetime = 1;                          // Time in minutes
+    public const int Lifetime = 5;                          // Time in minutes
 
     public static SymmetricSecurityKey GetSymmetricSecurityKey() {
         return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
@@ -15,6 +15,8 @@ public static class TokenConfig {
 
     public static TokenValidationParameters CreateValidationParameters() {
         return new TokenValidationParameters {
+            ClockSkew = TimeSpan.Zero,
+            
             ValidateIssuer = true,
             ValidIssuer = Issuer,
 
